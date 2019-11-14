b68dfa9198c2539e8e9e2909df24bb2d871ed30d
Angel Sanchez <asanchez@altimetrik.com>
2019-11-14 16:34:01 -0300

    Improvements

---
+++ src/_core/application.context.ts
@@ -0,0 +1,33 @@
+import { BaseUserModel } from './_auth/_models/user.model';
+import { Module } from './_base/module';
+import { securityContext } from './_base/_security/security.context';
+import App from './_base/app';
+import { RequestHandlerParams, ParamsDictionary } from 'express-serve-static-core';
+import configEnv from '../config/config';
+
+export const ApplicationContext = {
+  app: null,
+  security: securityContext,
+  init:<M extends Module, U extends BaseUserModel>(config: {
+    modules: (new () => M)[];
+    middleware?: (() => RequestHandlerParams<ParamsDictionary>)[];
+    security?: {userModel: any};
+  }) => {
+    configEnv();
+    this.app = new App();
+    if (config.security) {
+      securityContext.set(config.security);
+    }
+    try {
+      this.app.init({
+        modules: config.modules,
+        middleware: config.middleware
+      }).then((application: App) => {
+        this.app = application;
+        this.app.listen();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,15 @@
 import { UserModel } from './modules/user/models/user.model';
-import { securityContext } from './_core/_base/_security/security.context';
 import { UserModule } from './modules/user/user.module';
 import 'reflect-metadata';
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
-import configEnv from './config/config';
-import App from './_core/_base/app';
 import { TestModule } from './modules/test/test.module';
+import { ApplicationContext } from './_core/application.context';
 
-configEnv();
-securityContext.set({
-  userModel: UserModel as any
+ApplicationContext.init({
+  modules: [TestModule, UserModule],
+  middleware: [express.json, cookieParser],
+  security: {
+    userModel: UserModel as any
+  }
 });
-let app: App = new App();
-try {
-  app
-    .init({
-      modules: [TestModule, UserModule],
-      middleware: [express.json, cookieParser]
-    })
-    .then((application: App) => {
-      app = application;
-      app.listen();
-    });
-} catch (error) {
-  console.log(error);
-}
